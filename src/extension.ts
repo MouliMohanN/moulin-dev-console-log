8cf173002a7ebd9a2f94ae4938aae21303e01c4d
MouliMohanN <moulimohann@gmail.com>
2025-06-26 16:46:11 +0530

    updated extension.ts code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,126 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { parse } from '@babel/parser';
+import traverse from '@babel/traverse';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand('contextualConsoleLog.insertLog', async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "moulin-dev-console-log" is now active!');
+    const doc = editor.document;
+    const pos = editor.selection.active;
+    const code = doc.getText();
+    const fileName = doc.fileName.split(/\/|\\/).pop() || 'Unknown';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('moulin-dev-console-log.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from contextual-console-log!');
-	});
+    const ast = parse(code, {
+      sourceType: 'module',
+      plugins: ['jsx', 'typescript', 'classProperties', 'decorators-legacy']
+    });
 
-	context.subscriptions.push(disposable);
+    let isInsideRegularFunction = false;
+    let isInsideReactComponent = false;
+    let funcName: string | null = null;
+    let funcArgs: string[] = [];
+    let compName: string | null = null;
+    let props: string[] = [];
+    let states: string[] = [];
+    let refs: string[] = [];
+    let contexts: string[] = [];
+    let reducers: string[] = [];
+    let locals: string[] = [];
+
+    traverse(ast, {
+      enter(path) {
+        const node = path.node;
+        if (!node.loc) return;
+
+        if (positionIn(node.loc, pos, doc)) {
+          if (path.isFunctionDeclaration() || path.isFunctionExpression() || path.isArrowFunctionExpression()) {
+            const parent = path.parent;
+            const name = (node as any).id?.name ||
+              (parent.type === 'VariableDeclarator' && (parent as any).id?.name) ||
+              'anonymous';
+            isInsideRegularFunction = true;
+            funcName = name;
+            funcArgs = (node as any).params?.map((p: any) => p.name || doc.getText(p)) || [];
+          }
+
+          if (
+            path.isFunctionDeclaration() ||
+            path.isFunctionExpression() ||
+            path.isArrowFunctionExpression()
+          ) {
+            const returnNode = (node as any).body.body?.find((n: any) => n.type === 'ReturnStatement');
+            if (returnNode) {
+              compName = (node as any).id?.name ||
+                (path.parent.type === 'VariableDeclarator' && (path.parent as any).id.name) ||
+                'AnonymousComponent';
+              isInsideReactComponent = true;
+
+              const rawProps = (node as any).params || [];
+              props = rawProps.map((p: any) => p.name || doc.getText(p));
+            }
+          }
+        }
+
+        if (path.isVariableDeclarator() && path.node.init?.type === 'CallExpression') {
+          const callee = (path.node.init.callee as any).name;
+          const name = (path.node.id as any).name;
+          if (callee === 'useState') states.push(name);
+          if (callee === 'useRef') refs.push(name);
+          if (callee === 'useContext') contexts.push(name);
+          if (callee === 'useReducer') {
+            const [stateVar, dispatchVar] = (path.node.id as any).elements.map((e: any) => e.name);
+            reducers.push(`${stateVar}: ${stateVar}, ${dispatchVar}: ${dispatchVar}`);
+          }
+        }
+
+        if (path.isVariableDeclarator() && path.node.init && positionIn(path.node.loc, pos, doc)) {
+          const name = (path.node.id as any).name;
+          if (
+            !states.includes(name) &&
+            !refs.includes(name) &&
+            !contexts.includes(name) &&
+            !reducers.some(r => r.includes(name))
+          ) {
+            locals.push(name);
+          }
+        }
+      }
+    });
+
+    let logLine = '';
+
+    if (isInsideRegularFunction) {
+      const argsStr = funcArgs.length ? `{ args: { ${funcArgs.join(', ')} } }` : `{}`;
+      logLine = `console.log('[${fileName} > ${funcName}]', ${argsStr});`;
+    } else if (isInsideReactComponent && compName) {
+      const logParts: string[] = [];
+
+      if (props.length) logParts.push(`props: { ${props.join(', ')} }`);
+      if (states.length) logParts.push(`state: { ${states.join(', ')} }`);
+      if (refs.length) logParts.push(`refs: { ${refs.map(r => `${r}: ${r}.current`).join(', ')} }`);
+      if (contexts.length) logParts.push(`context: { ${contexts.join(', ')} }`);
+      if (reducers.length) logParts.push(`reducer: { ${reducers.join(', ')} }`);
+      if (locals.length) logParts.push(`locals: { ${locals.join(', ')} }`);
+
+      logLine = `console.log('[${fileName} > ${compName}]', {\n  ${logParts.join(',\n  ')}\n});`;
+    } else {
+      logLine = `console.log('[${fileName}]');`;
+    }
+
+    await editor.edit(editBuilder => {
+      editBuilder.insert(pos, logLine + '\n');
+    });
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function positionIn(loc: any, pos: vscode.Position, doc: vscode.TextDocument) {
+  const start = new vscode.Position(loc.start.line - 1, loc.start.column);
+  const end = new vscode.Position(loc.end.line - 1, loc.end.column);
+  return pos.isAfterOrEqual(start) && pos.isBeforeOrEqual(end);
+}
+
 export function deactivate() {}
