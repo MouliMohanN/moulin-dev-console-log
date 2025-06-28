f4d32b58c4a697407edae8130f4b379371a07d0e
MouliMohanN <moulimohann@gmail.com>
2025-06-28 16:18:57 +0530

    feat: Improve nested function argument detection and useMemo handling
    This commit refactors the AST traversal in `src/parser.ts` to accurately identify the innermost function containing the cursor. It now correctly extracts arguments for all parameters of nested functions.
    
    Additionally, it ensures that variables assigned with `useMemo` are included in the `locals` array, as per user request, while still excluding `useCallback` variables.

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,5 @@
 import { parse } from '@babel/parser';
-import traverse from '@babel/traverse';
+import traverse, { NodePath } from '@babel/traverse';
 import * as t from '@babel/types';
 import * as vscode from 'vscode';
 import { logger } from './logger';
@@ -21,13 +21,13 @@ export function parseCodeContext(code: string, cursor: vscode.Position, doc: vsc
     plugins: ['jsx', 'typescript'],
   });
 
-  let context: CodeContext | null = null;
-  logger.log('parser.ts ~ parseCodeContext ~ before traverse ~ ast', { ast });
+  let innermostFunctionPath: any = null; // Using 'any' for simplicity, will be a NodePath
 
   traverse(ast, {
     enter(path) {
       const node = path.node;
-      if (!node.loc) {return;}
+
+      if (!node.loc) { return; }
 
       if (
         positionIn(node.loc, cursor, doc) &&
@@ -36,122 +36,124 @@ export function parseCodeContext(code: string, cursor: vscode.Position, doc: vsc
           t.isFunctionExpression(node) ||
           t.isClassMethod(node))
       ) {
-        const name =
-          (node as any).id?.name ||
-          ((node as any).key?.name as string) ||
-          (path.parent.type === 'VariableDeclarator' && (path.parent as any).id?.name) ||
-          'anonymous';
-
-        // JSX detection
-        let isComponent = false;
-        path.traverse({
-          JSXElement() {
-            isComponent = true;
-            path.stop();
-          },
-          JSXFragment() {
-            isComponent = true;
-            path.stop();
-          },
-        });
-
-        const variables: VariableBuckets = {
-          props: [],
-          state: [],
-          refs: [],
-          context: [],
-          reducers: [],
-          locals: [],
-        };
-
-        let args: string[] = [];
-        const firstParam = node.params?.[0];
-
-        if (firstParam && (t.isObjectPattern(firstParam) || t.isIdentifier(firstParam))) {
-          args = extractVariableNames(firstParam);
+        // If this is the first function found, or if this function is more "inner"
+        // than the previously found one, update innermostFunctionPath.
+        if (!innermostFunctionPath || (
+            node.loc.start.line >= innermostFunctionPath.node.loc!.start.line &&
+            node.loc.end.line <= innermostFunctionPath.node.loc!.end.line &&
+            (node.loc.end.line - node.loc.start.line < innermostFunctionPath.node.loc!.end.line - innermostFunctionPath.node.loc!.start.line ||
+             (node.loc.end.line - node.loc.start.line === innermostFunctionPath.node.loc!.end.line - innermostFunctionPath.node.loc!.start.line &&
+              node.loc.start.column >= innermostFunctionPath.node.loc!.start.column &&
+              node.loc.end.column <= innermostFunctionPath.node.loc!.end.column))
+        )) {
+          innermostFunctionPath = path;
         }
-
-        logger.log('parser.ts ~ parseCodeContext ~ traverse ~ enter', {
-          name,
-          isComponent,
-          args,
-          nodeParams: (node as any).params,
-        });
-
-        if (isComponent) {
-          variables.props = args;
-        }
-
-        const insertPos = findReturnInsertPosition(node, doc);
-
-        logger.log('parser.ts ~ parseCodeContext ~ traverse ~ insertPos', { insertPos, firstParam, variables });
-
-        // Create a new context object for the current function/component
-        const currentContext: CodeContext = {
-          type: isComponent ? 'react' : 'function',
-          name,
-          args,
-          insertPos,
-          variables: {
-            ...variables,
-          },
-        };
-
-        // Update the overall context if this is a more specific (deeper) context
-        // or the first context found.
-        if (node.loc && positionIn(node.loc, cursor, doc)) {
-          context = currentContext;
-        }
-
-        path.traverse({
-          VariableDeclarator(inner) {
-            const id = inner.node.id;
-            const init = inner.node.init;
-
-            if (!t.isIdentifier(id) && !t.isArrayPattern(id) && !t.isObjectPattern(id)) {return;}
-
-            const names = extractVariableNames(id);
-
-            logger.log('parser.ts ~ VariableDeclarator', { id, init, names });
-
-            if (t.isCallExpression(init)) {
-              const callee = (init.callee as any).name || (init.callee as any).property?.name;
-
-              switch (callee) {
-                case 'useState':
-                  if (t.isArrayPattern(id) && id.elements[0] && t.isIdentifier(id.elements[0])) {
-                    currentContext.variables.state.push(id.elements[0].name);
-                  }
-                  break;
-                case 'useRef':
-                  names.forEach((n) => currentContext.variables.refs.push(n));
-                  break;
-                case 'useContext':
-                  names.forEach((n) => currentContext.variables.context.push(n));
-                  break;
-                case 'useReducer':
-                  if (names.length >= 2) {
-                    currentContext.variables.reducers.push(`${names[0]}: ${names[0]}, ${names[1]}: ${names[1]}`);
-                  }
-                  break;
-                case 'useCallback':
-                case 'useMemo':
-                  // Do nothing, as these return functions and should not be logged as locals
-                  break;
-                default:
-                  names.forEach((n) => currentContext.variables.locals.push(n));
-              }
-            } else if (!t.isArrowFunctionExpression(init) && !t.isFunctionExpression(init)) {
-              names.forEach((n) => currentContext.variables.locals.push(n));
-            }
-          },
-        });
-        logger.log('parser.ts ~ parseCodeContext ~ final context', context);
       }
     },
   });
 
-  return context;
+  if (innermostFunctionPath) {
+    const node = innermostFunctionPath.node;
+    const path = innermostFunctionPath; // Use the path of the innermost function
+
+    const name =
+      (node as any).id?.name ||
+      ((node as any).key?.name as string) ||
+      (path.parent.type === 'VariableDeclarator' && (path.parent as any).id?.name) ||
+      'anonymous';
+
+    // JSX detection
+    let isComponent = false;
+    path.traverse({
+      JSXElement() {
+        isComponent = true;
+        path.stop();
+      },
+      JSXFragment() {
+        isComponent = true;
+        path.stop();
+      },
+    });
+
+    const variables: VariableBuckets = {
+      props: [],
+      state: [],
+      refs: [],
+      context: [],
+      reducers: [],
+      locals: [],
+    };
+
+    let args: string[] = [];
+    if (node.params) {
+      node.params.forEach((param: t.LVal) => {
+        if (t.isObjectPattern(param) || t.isIdentifier(param) || t.isArrayPattern(param)) {
+          args.push(...extractVariableNames(param));
+        }
+      });
+    }
+
+    if (isComponent) {
+      variables.props = args;
+    }
+
+    const insertPos = findReturnInsertPosition(node, doc);
+
+    const currentContext: CodeContext = {
+      type: isComponent ? 'react' : 'function',
+      name,
+      args,
+      insertPos,
+      variables: {
+        ...variables,
+      },
+    };
+
+    // Now, traverse only within the innermost function's scope for VariableDeclarator
+    innermostFunctionPath.traverse({
+      VariableDeclarator(inner: NodePath<t.VariableDeclarator>) {
+        const id = inner.node.id;
+        const init = inner.node.init;
+
+        if (!t.isIdentifier(id) && !t.isArrayPattern(id) && !t.isObjectPattern(id)) { return; }
+
+        const names = extractVariableNames(id);
+
+        if (t.isCallExpression(init)) {
+          const callee = (init.callee as any).name || (init.callee as any).property?.name;
+
+          switch (callee) {
+            case 'useState':
+              if (t.isArrayPattern(id) && id.elements[0] && t.isIdentifier(id.elements[0])) {
+                currentContext.variables.state.push(id.elements[0].name);
+              }
+              break;
+            case 'useRef':
+              names.forEach((n) => currentContext.variables.refs.push(n));
+              break;
+            case 'useContext':
+              names.forEach((n) => currentContext.variables.context.push(n));
+              break;
+            case 'useReducer':
+              if (names.length >= 2) {
+                currentContext.variables.reducers.push(`${names[0]}: ${names[0]}, ${names[1]}: ${names[1]}`);
+              }
+              break;
+            case 'useCallback':
+              // Do nothing, as these return functions and should not be logged as locals
+              break;
+            default:
+              names.forEach((n) => currentContext.variables.locals.push(n));
+          }
+        } else if (!t.isArrowFunctionExpression(init) && !t.isFunctionExpression(init)) {
+          names.forEach((n) => currentContext.variables.locals.push(n));
+        }
+      },
+    });
+    return currentContext;
+  }
+
+  return null;
 }
 
 function extractVariableNames(id: t.Identifier | t.ArrayPattern | t.ObjectPattern): string[] {
@@ -169,8 +171,6 @@ function extractVariableNames(id: t.Identifier | t.ArrayPattern | t.ObjectPatter
     id.properties.forEach((prop) => {
       if (t.isObjectProperty(prop) && t.isIdentifier(prop.value)) {
         names.push(prop.value.name);
-      } else if (t.isRestElement(prop) && t.isIdentifier(prop.argument)) {
-        names.push(prop.argument.name);
       }
     });
   }
